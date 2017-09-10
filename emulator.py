e72345cbf34d0ed849e6a957ffba5c15f7342aa3
dude2093350 <31752151+dude2093350@users.noreply.github.com>
2017-09-10 11:46:03 -0700

    Update emulator.py

--- emulator.py
+++ emulator.py
@@ -1,4 +1,16 @@
-emulator.py
-x = 9
-p = 3
-print(x+p)
+//* emulator.py is property of Dude2093350 and cTechnologies
+
+Copyright (c) Dude2093350
+Copyright (c) cTechnolagies
+
+Access.gitignore.footpath.extract = false
+
+X = 1
+A = 2
+C = 3
+B = 4
+Y = 5
+Print ('y+c')
+If.dlc.extract.root.run = server.full.transferserver = false
+Or
+If.dlc.extract.root.run.play.error = true
